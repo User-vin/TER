4b35ee6458bc00721ecaf796bd44de004cbca919
scott barker <119344955+etudiantdescartes@users.noreply.github.com>
2023-03-30 13:25:40 +0200

    Add files via upload

--- plotxlsx.py
+++ plotxlsx.py
@@ -12,7 +12,7 @@ def all_images(path, col):#col: +0,1,2,3
     x = df['01-01.jpg'].values
     x1 = np.arange(x.size)
 
-    # Create the plot
+
     fig, ax = plt.subplots()
     i = 0
     for val in y:
@@ -23,7 +23,7 @@ def all_images(path, col):#col: +0,1,2,3
         ax.scatter(x1, y1, s=sizes, marker='s')
         i += 1
 
-    # Add labels and title
+
     ax.set_xlabel('')
     ax.set_ylabel('')
     ax.set_title('Comparaisons des vecteurs caractéristiques')
@@ -61,7 +61,7 @@ def single_image(name, path):
     #ax.plot(x, y_rgb, label='rgb')
     ax.plot(x, y_gray, label='gray')
     
-    # Add labels and title
+    
     ax.set_xlabel('images')
     ax.set_ylabel('distance')
     ax.set_title(f'comparaison {name}')
@@ -98,18 +98,108 @@ def labels_kmeans(path):
     
     
     
+def precision_recall_f1(path):
+    df = pd.read_excel(path)
+    num_columns = len(df.columns)
+    
+    for i in range(0, num_columns, 4):
+        
+        sub_df = df.iloc[:, i:i+4]
+        new_dfs = []
+        for k in range(0, num_columns, 4):
+            sub_df = df.iloc[:, k:k+4]
+            
+            for j in range(3):
+                new_cols = [sub_df.columns[0], sub_df.columns[j+1]]
+                new_df = sub_df[new_cols]
+                new_df = new_df.sort_values(by=new_cols[1], ascending=False)
+                new_dfs.append(new_df)
+                
+    sum_color_space_precision_gray = 0
+    sum_color_space_recall_gray = 0
+    
+    sum_color_space_precision_opp = 0
+    sum_color_space_recall_opp = 0
+    
+    sum_color_space_precision_rgb = 0
+    sum_color_space_recall_rgb = 0
+    
+                
+    for i in range(len(new_dfs)):
+        plot_df = new_dfs[i].iloc[1:6]
+        s = 0
+        yp = []
+        name = plot_df.columns[0].split('.')[0]
+        
+        for j in range(5):
+            #classe = plot_df.iloc[:, 0].values[j].split('.')[0].split('-')[0]
+            #s += 1 if classe == name.split('-')[0] else 0
+            classe = plot_df.iloc[:, 0].values[j].split('.')[0].split('__')[0]
+            s += 1 if classe == name.split('__')[0] else 0
+            yp.append(s/(j+1))
+            
+        s = 0
+        yr = []
+        for j in range(5):
+            #classe = plot_df.iloc[:, 0].values[j].split('.')[0].split('-')[0]
+            #s += 1 if classe == name.split('-')[0] else 0
+            classe = plot_df.iloc[:, 0].values[j].split('.')[0].split('__')[0]
+            s += 1 if classe == name.split('__')[0] else 0
+            yr.append(s/5)
+            
+        color_space = plot_df.columns[1].split('.')[0]    
+        
+        if color_space == 'gray':
+            sum_color_space_precision_gray += yp[4]
+            sum_color_space_recall_gray += yr[4]
+            
+        elif color_space == 'opp_colors_uint8':
+            sum_color_space_precision_opp += yp[4]
+            sum_color_space_recall_opp += yr[4]
+        
+        elif color_space == 'rgb':
+            sum_color_space_precision_rgb += yp[4]
+            sum_color_space_recall_rgb += yr[4]
+        
+            
+        plt.plot(plot_df.iloc[:, 0], yp, label='precision')
+        plt.plot(plot_df.iloc[:, 0], yr, label='recall')
+        plt.legend()
+        
+        
+        plt.title(f"Plot {name} {color_space}")
+        text = 'precision: {:.2f}\nrecall: {:.2f}'.format(yp[4], yr[4])
+        plt.text(1, 1, text, transform=plt.gca().transAxes, va='top', ha='right')
+        plt.xlabel('Images')
+        plt.ylabel('precision / recall')
+        plt.ylim(-0.1, 1.1)
+        
+        plt.savefig(f'C:/Users/scott/OneDrive/Bureau/prec_rec/coil_100/new/sub_dataset/normal/{color_space}/{name}')
+
+        plt.show()
+        
+        
+    print(f'gray average precison: {sum_color_space_precision_gray/(len(new_dfs)/3)}\naverage recall: {sum_color_space_recall_gray/(len(new_dfs)/3)}')
+    print(f'opp average precison: {sum_color_space_precision_opp/(len(new_dfs)/3)}\naverage recall: {sum_color_space_recall_opp/(len(new_dfs)/3)}')
+    print(f'rgb average precison: {sum_color_space_precision_rgb/(len(new_dfs)/3)}\naverage recall: {sum_color_space_recall_rgb/(len(new_dfs)/3)}')
+    
+
+    
+    
+    
 classes = r'C:\Users\scott\OneDrive\Bureau\papillons\gray\kmeans\classes.xlsx'
 #labels_kmeans(classes)
 
 path = r'C:\Users\scott\OneDrive\Bureau\ter\results.xlsx'
 
-for i in glob(r'C:\Users\scott\OneDrive\Bureau\imgs\*'):
-    name = i.split('\\')[-1]
-    single_image(name, path)
-path = r'C:\Users\scott\OneDrive\Bureau\papillons\results.xlsx'    
-#Uall_images(path, 1)
+#for i in glob(r'C:\Users\scott\OneDrive\Bureau\imgs\*'):
+#    name = i.split('\\')[-1]
+#    single_image(name, path)
+#path = r'C:\Users\scott\OneDrive\Bureau\papillons\results.xlsx'    
+#all_images(path, 1)
 
-
+path = r'C:\Users\scott\OneDrive\Bureau\ter\results.xlsx'
+precision_recall_f1(path)
 
 
 
--- ter.py
+++ ter.py
@@ -12,8 +12,6 @@ def caracteristic_vector_opponent_color_uint8(path, path_seg):
     """
     Lit une image dans le chemin 'path', convertir l'espace de couleur comme dans le papier (opponent colors) et retourne le vecteur caractéristique
     """
-    if path.split('\\')[-1] != path_seg.split('\\')[-1]:
-        print('EEEERRRRRRRRROOOOOOOOORRRRRRRRR')
     img = cv2.imread(path)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     
@@ -26,22 +24,21 @@ def caracteristic_vector_opponent_color_uint8(path, path_seg):
     #on réassemble chaque canal pour reformer l'image
     nimg = np.dstack((rg, by, wb))
     
+    data = nimg.reshape((-1,3))
+    
     mask = cv2.imread(path_seg)[:,:,0]
     indices = np.where(mask)
     object_pixels = nimg[indices]
     
     #histogramme
-    hist, edges = np.histogramdd(object_pixels, bins=(16,16,8), range=((0,256),(0,256),(0,256)))
+    hist, edges = np.histogramdd(data, bins=(16,16,8), range=((0,256),(0,256),(0,256)))
     
     #vecteur caracteritique en linearisant l'histogramme 3d
     vec = hist.flatten()
     return vec
 
-
+"""
 def caracteristic_vector_opponent_color_int16(path, path_seg):
-    """
-    Pareil en int16 (pour valeurs négatives et supérieures à 255)
-    """
     img = cv2.imread(path)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB).astype(np.int16)
     mask = cv2.imread(path_seg)[:,:,0]
@@ -58,7 +55,7 @@ def caracteristic_vector_opponent_color_int16(path, path_seg):
     hist, edges = np.histogramdd(object_pixels, bins=(16,16,8), range=((-255,256),(-2*255,2*255),(0,3*255)))
     vec = hist.flatten()
     return vec
-
+"""
 
 
 def caracteristic_vector_rgb(path, path_seg):
@@ -68,11 +65,13 @@ def caracteristic_vector_rgb(path, path_seg):
     img = cv2.imread(path)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     
+    data = img.reshape((-1,3))
+    
     mask = cv2.imread(path_seg)[:,:,0]
     indices = np.where(mask)
     object_pixels = img[indices]
     
-    hist, edges = np.histogramdd(object_pixels, bins=(16,16,16), range=((0,256),(0,256),(0,256)))
+    hist, edges = np.histogramdd(data, bins=(16,16,16), range=((0,256),(0,256),(0,256)))
     vec = hist.flatten()
     return vec
 
@@ -87,7 +86,7 @@ def caracteristic_vector_gray(path, path_seg):
     indices = np.where(mask)
     object_pixels = img[indices]
     
-    hist, edges = np.histogram(object_pixels, bins=(200), range=(0,256))
+    hist, edges = np.histogram(img.flatten(), bins=(255), range=(0,256))
     return hist
 
 
@@ -97,7 +96,7 @@ def comparison(to_classify, to_classify_seg, imgs, imgs_seg, carac_vector, col_n
     Renvoie une liste de listes qui contient le nom de l'image qu'on compare à 'to_classify' et le score. La liste est triée en fonction du score
     """
     color_vec1 = carac_vector(to_classify, to_classify_seg)#recuperation du vecteur caractéristique
-    color_vec1 = color_vec1 / sum(color_vec1)#normalisation
+    color_vec1 = color_vec1 / max(color_vec1)#normalisation
     
     
     name = to_classify.split('\\')[-1]
@@ -106,7 +105,7 @@ def comparison(to_classify, to_classify_seg, imgs, imgs_seg, carac_vector, col_n
     for img, img_seg in zip(imgs, imgs_seg):
         color_vec2 = carac_vector(img, img_seg)
         color_vec2 = color_vec2 / sum(color_vec2)
-        color_intersection = sum(np.minimum(color_vec1, color_vec2))
+        color_intersection = sum(np.minimum(color_vec1, color_vec2))/sum(np.maximum(color_vec1, color_vec2))
         df.loc[len(df.index)] = [img.split('\\')[-1], round(color_intersection, 3)]
     df = df.sort_values(by=name, key=natsort_keygen())
     return df
@@ -120,13 +119,13 @@ def write_xls(imgs, imgs_seg, path, path_seg, xlsx):
     #results_opp16 = comparison(path, path_seg, imgs, imgs_seg, caracteristic_vector_opponent_color_int16, 'opp_colors_int16')
     
     #résultats en rgb (sans modifier l'espace)
-    #results_rgb = comparison(path, path_seg, imgs, imgs_seg, caracteristic_vector_rgb, 'rgb')
+    results_rgb = comparison(path, path_seg, imgs, imgs_seg, caracteristic_vector_rgb, 'rgb')
     
     #résultats en nuances de gris
     results_gray = comparison(path, path_seg, imgs, imgs_seg, caracteristic_vector_gray, 'gray')
     
     #results_opp8['opp_colors_int16'] = results_opp16['opp_colors_int16']
-    #results_opp8['rgb'] = results_rgb['rgb']
+    results_opp8['rgb'] = results_rgb['rgb']
     results_opp8['gray'] = results_gray['gray']
     
     return results_opp8
@@ -135,11 +134,11 @@ def write_xls(imgs, imgs_seg, path, path_seg, xlsx):
 
 if __name__ == "__main__":
 
-    imgs_set1 = natsorted(glob(r'C:\Users\scott\OneDrive\Bureau\imgs\*.png'))
-    imgs_set1_seg = natsorted(glob(r'C:\Users\scott\OneDrive\Bureau\masks\*.png'))
+    imgs_set1 = natsorted(glob(r'C:\Users\scott\OneDrive\Bureau\coil_100\2e_dataset_imgs\*.png'))
+    imgs_set1_seg = natsorted(glob(r'C:\Users\scott\OneDrive\Bureau\coil_100\2e_dataset_masks\*.png'))
     
-    imgs_set2 = natsorted(glob(r'C:\Users\scott\OneDrive\Bureau\imgs\*.png'))
-    imgs_set2_seg = natsorted(glob(r'C:\Users\scott\OneDrive\Bureau\masks\*.png'))
+    imgs_set2 = natsorted(glob(r'C:\Users\scott\OneDrive\Bureau\coil_100\2e_dataset_imgs\*.png'))
+    imgs_set2_seg = natsorted(glob(r'C:\Users\scott\OneDrive\Bureau\coil_100\2e_dataset_masks\*.png'))
     
     
     xlsx = 'results.xlsx'
